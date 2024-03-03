005d7ad8d70d83e243a32f8f402b528cf7e1652f
Matt Cooper <vtbassmatt@gmail.com>
2024-03-03 16:31:44 -0500

    hex storage

--- hexgrid/models.py
+++ hexgrid/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class HexCell(models.Model):
+    q = models.IntegerField()
+    r = models.IntegerField()
+    s = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(s=models.F('q') * -1 - models.F('r')),
+                name='coord_invariant',
+            ),
+            models.UniqueConstraint(
+                fields=['q', 'r', 's'],
+                name='unique_coords',
+            )
+        ]
+
+    def __str__(self):
+        return f"({self.q}, {self.r}, {self.s})"
+
+    @classmethod
+    def from_axial(cls, q: int, r: int):
+        return cls(q=q, r=r, s=-q-r)
