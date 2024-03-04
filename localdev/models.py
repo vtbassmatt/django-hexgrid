b3ff574d049d55c48cea8a2a654cef1e247d7400
Matt Cooper <vtbassmatt@gmail.com>
2024-03-04 15:43:21 -0500

    create some content for the local dev experience

--- localdev/models.py
+++ localdev/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from hexgrid.models import HexCell
+
+
+class Thing(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.ForeignKey(
+        HexCell,
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return self.name
--- localdev/views.py
+++ localdev/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from hexgrid.models import HexCell
+
+
+def home(request):
+    origin, is_new = HexCell.get_or_create_origin()
+    if is_new:
+        origin.save()
+
+    return render(
+        request,
+        'cell.html',
+        {
+            'cell': origin,
+        }
+    )
